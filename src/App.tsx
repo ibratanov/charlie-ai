63133f7653e6f8c9e497467fbdbfecf743ffceaa
Ivo <4753372+ibratanov@users.noreply.github.com>
2023-08-24 23:41:24 -0700

    Purify & parse the response, improve prompt

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import Footer from './components/Footer'
 import TextInput from './components/TextInput'
 import FeedbackModal from './components/FeedbackModal'
 
+import DOMPurify from 'dompurify'
+
 const App = () => {
   const [feedback, setFeedback] = useState('')
   const [isOpen, setIsOpen] = useState(false)
@@ -14,19 +16,26 @@ const App = () => {
     setLoading(true)
     setIsOpen(true)
     const prompt: string = `
-      You are a technical recruiter named Charlie. Your goal is to help candidates
-      land their dream job by assisting with their job search. Please be kind and
-      friendly, and give direct feedback on the resume provided. I'd like it to
-      perform well in applicant tracking systems and I'd like it to be easy to read.
+      You are an incredibly sharp technical recruiter named Charlie.
+      Your goal is to help candidates land their dream job by assisting with their job search.
+      You are always kind and friendly. Please give direct feedback on the resume provided.
+      I'd like the resume to perform well in applicant tracking systems and I'd like it to
+      impressive for hiring managers and recruiters. It should also be easy to read.
       Break your suggested changes up into actionable items in bullet form so I know
       what to address first. A few more things to keep in mind:
       - Please don't introduce yourself or say hello. We're already acquainted.
       - Don't recommend that I proof read for grammatical errors.
       - If you notice any spelling or grammatical errors, please list them.
-      - Please get very specific and provide a lot of feedback, looking at every line
+      - Please be very specific and provide a lot of feedback, looking at every line
       of the resume and recommending specific changes on how to improve sentences that
       aren't compelling enough.
-      - Please add a \n\n after each bullet point so that each bullet point is on a new line.
+      - Make sure you don't repeat yourself or give generic suggestions.
+
+      Please formate your response as if it were HTML code, such that it can be plugged into a React component and formatted accordingly. I'd like all resume suggestions returned as an ordered list using this format: <ol><li>(Suggestion)</li><li>(Suggestion2)</li>...</ol>
+      If you need to provide sub-points, please use an unordered list: <ul><li>(Sub-suggestion)</li><li>(Sub-suggestion2)</li>...</ul>
+      Please put any additional information you'd like to provide before or after the suggestions in a <div> tag.
+      Please use a <strong></strong> tag for titles of sections or to highlight any text you'd like to emphasize.
+      Due to the way the HTML is parsed, please don't use any other HTML tags.
       Thank you for your help!
       Here's my resume:\n\n
     `
@@ -40,10 +49,10 @@ const App = () => {
       body: JSON.stringify({
         model: 'gpt-3.5-turbo',
         messages: [
-          {"role": "system", "content": prompt + text + ''},
+          {"role": "system", "content": '{ "prompt": ' + prompt + text + '"output_format": "html" }'},
         ],
-        temperature: 0.5,
-        frequency_penalty: 0.8
+        temperature: 0.2,
+        frequency_penalty: 0.8,
       })
     }
 
@@ -53,7 +62,9 @@ const App = () => {
 
     const data = json.choices[0].message.content
 
-    setFeedback(data)
+    const cleanData = DOMPurify.sanitize(data)
+
+    setFeedback(cleanData)
     setLoading(false)
   }
 
@@ -62,7 +73,7 @@ const App = () => {
   }
 
   return (
-    <Box bg='blue.400' color='white' height='100vh' paddingTop={130}>
+    <Box bg='blue.400' color='white' h='100vh' >
       <Container maxW='3xl' centerContent>
         <Header />
         <TextInput generateFeedback={generateFeedback} />
